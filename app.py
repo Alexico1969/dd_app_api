57b0b84929c9ee5542b118c71024b0e93ecad518
Alex van Winkel <alexicoo@gmail.com>
2022-05-31 00:50:00 -0400

    temp change for demo

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/times", methods=["GET"])
 @cross_origin()
 def message():
-    return jsonify("July 1st : 12:00 am (EST)   9:00 am (PST)|July 2nd : 12:00 am (EST)   9:00 am (PST)|July 3rd : 12:00 am (EST)   9:00 am (PST)|July 4th : 12:00 am (EST)   9:00 am (PST)")
-    
-
+    #return jsonify("July 1st : 12:00 am (EST)   9:00 am (PST)|July 2nd : 12:00 am (EST)   9:00 am (PST)|July 3rd : 12:00 am (EST)   9:00 am (PST)|July 4th : 12:00 am (EST)   9:00 am (PST)")
+    return jsonify("Carline is the sweetest!")
 
 if __name__=='__main__':
     app.run(debug=True)
