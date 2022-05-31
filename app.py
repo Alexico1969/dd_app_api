b3234b7936520794bbe902cb85fa9c04d2e358a7
Alex van Winkel <alexicoo@gmail.com>
2022-05-30 21:15:32 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app= Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/times", methods=["GET"])
+@cross_origin()
+def message():
+    return jsonify("""July 1st : 12:00 am (EST)   9:00 am (PST)|July 2nd : 12:00 am (EST)   9:00 am (PST)""")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
